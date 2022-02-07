3ee0fb58cbbd17934daa6954d9f8b4f41f1856e5
YammyShep <jdbuelow@gmail.com>
2022-02-07 10:53:15 -0600

    Ditched the slf4j logger, itll just make console output harder to read

--- src/main/java/com/jbuelow/cs358/ballot/BallotCounter.java
+++ src/main/java/com/jbuelow/cs358/ballot/BallotCounter.java
@@ -6,41 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 class BallotCounter {
-	
-	public final static Logger log = LoggerFactory.getLogger(BallotCounter.class);
 
-	public static void main(String[] args) {
-		// Give the startup log
-		log.info("\n\nRanked Ballot Election Counter\nby Jacob Buelow\n\nStarting...\n");
+	public static void main(String[] args) {		
+		// Give the startup message
+		System.out.println("Runoff Election Ballot Counter");
+		System.out.println();
 		
-		String filename = null;
-		
-		// Parse command line arguments and gather filename of dataset
-		if (Arrays.asList(args).indexOf("-i") >= 0) {
-			//Interactive mode
-			log.info("Interractive flag set");
-			//TODO
-		} else if (Arrays.asList(args).indexOf("-f") >= 0) {
-			//File mode
-			log.info("File mode flag set");
-			try {
-				filename = args[Arrays.asList(args).indexOf("-f") + 1];
-			} catch (ArrayIndexOutOfBoundsException e) {
-				log.error("No filename given! - exiting!");
-				return;
-			}
-		} else {
-			//No mode specified or given. quit and error
-			log.error("File nor interractive mode specified. run with -i to enter interractive mode, or use -f <filename> - exiting!");
-			return;
-		}
-		
-		//edgecase that shouldnt happen if i did my job right
-		if (filename == null) {
-			log.error("Unable to parse filename! - exiting!");
-			return;
-		}
-		
+				
 		
 	}
 
